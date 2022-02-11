d9ed2e42162c9eb2ed2b9da3e69f1e038b7d5576
Daniel Lachica <daniel.lachica82@gmail.com>
2022-02-11 13:28:08 +0800

    Set up Vue3

---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+    export default {
+        setup: () => ({
+            title: 'Hello, World! This is my Vue3 App',
+        }),
+    }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+import WelcomeBanner from './Components/Welcome.vue';
+
+const app = createApp({});
+
+app.component('welcome-banner', WelcomeBanner);
+
+app.mount('#app');
